35d0a7969113bd1fedec50be514b2dca51ad98c5
Aditya Jonathan <128442638+inijona21@users.noreply.github.com>
2024-06-13 02:14:17 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ const { Server } = require("socket.io")
 const server = http.createServer(app)
 const io = new Server(server, {
 	cors: {
-		origin: "*",
+		origin: "https://le-code.vercel.app/",
 	},
 })
 
