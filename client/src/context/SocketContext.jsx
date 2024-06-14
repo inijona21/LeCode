b8fcaaaea0d796f1bed5e0994d5999301e257496
Aditya Jonathan <128442638+inijona21@users.noreply.github.com>
2024-06-15 00:09:35 +0700

    Update SocketContext.jsx

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -6,7 +6,7 @@ import { createContext, useCallback, useEffect, useMemo } from "react"
 import { toast } from "react-hot-toast"
 import { io } from "socket.io-client"
 
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+const BACKEND_URL = import.meta.env.BACKEND_URL
 
 const SocketContext = createContext()
 
