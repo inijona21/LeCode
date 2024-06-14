a11a7948f5c3663bf7aff3d66cf277bbf46ef19d
Aditya Jonathan <128442638+inijona21@users.noreply.github.com>
2024-06-15 00:22:50 +0700

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
 import { io } from "socket.io-client";
 
 // Ganti dengan URL server Anda
-const backendUrl = "https://le-code-server.vercel.app";
+const backendUrl = "http://le-code-server.vercel.app";
 
 const socket = io(backendUrl, {
     transports: ["websocket", "polling"],
