4edaecf7815443974307afd694ac03a58251cd3f
Aditya Jonathan <128442638+inijona21@users.noreply.github.com>
2024-06-15 23:37:49 +0700

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,23 +3,23 @@ import FormComponent from "@/components/forms/FormComponent";
 // import Footer from "@/components/common/Footer";
 
 function HomePage() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center gap-15" style={{ backgroundColor: 'black' }}>
-      <div className="my-10 flex h-full min-w-full flex-col items-center justify-evenly sm:flex-row sm:pt-0">
-        <div className="flex w-full items-center justify-center sm:w-1/3 pl-40">
-          <FormComponent className="w-full max-w-md" />
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center gap-16 bg-black">
+            <div className="my-12 flex h-full min-w-full flex-col-reverse items-center justify-evenly sm:flex-row sm:pt-0">
+                <div className="flex w-full items-center justify-center sm:w-1/2">
+                    <FormComponent />
+                </div>
+                <div className="flex w-full animate-up-down justify-center sm:w-1/2 sm:pl-4">
+                    <img
+                        src={LeCode}
+                        alt="LeCode"
+                        className="mx-auto w-[600px] sm:w-[800px]"
+                    />
+                </div>
+            </div>
+            {/* <Footer /> */}
         </div>
-        <div className="flex w-full animate-up-down justify-center sm:w-1/2 sm:pl-15 pl-20">
-          <img
-            src={LeCode}
-            alt="LeCode"
-            className="mx-auto w-[700px] sm:w-[900px]"
-          />
-        </div>
-      </div>
-      {/* <Footer /> */}
-    </div>
-  );
+    )
 }
 
 export default HomePage;
